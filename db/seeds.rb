dc73171a8e200cc8f5f1e35985664b3fcdd697d1
Elif Sezgin <elifmsezgin@gmail.com>
2017-01-16 22:08:05 -0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Group.find_by_title('Evolutionists').delete
+
+Group.create(title: 'Evolutionists', description: 'Meet other local evolutionists to discuss the latest news and theories.', city_name: 'San Francisco', image_url: "http://res.cloudinary.com/datsbxfvs/image/upload/v1484632895/evolution-wide-wallpaper-20363_de3u4z.jpg")
+
+
 Group.create(title: "Computer Scientists", description: "Meet other local computer scientists to discuss the latest news and theories.", city_name: "San Francisco", image_url: "http://res.cloudinary.com/datsbxfvs/image/upload/v1484353544/compsc_n2ryhb.png")
 
-
-Group.create(title: 'Evolutionists', description: 'Meet other local evolutionists to discuss the latest news and theories.', city_name: 'San Francisco', image_url: "http://res.cloudinary.com/datsbxfvs/image/upload/v1484352068/Unknown_bhzijt.png")
 
 Group.create(title: 'Data Scientists', description: 'Meet other local data scientists to discuss the latest news and theories.', city_name: 'San Francisco', image_url: "http://res.cloudinary.com/datsbxfvs/image/upload/v1484352124/Unknown-3_gtowm6.jpg")
 
