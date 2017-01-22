c392a08f0cf63638252988fc58b59a431b842206
Elif Sezgin <elifmsezgin@gmail.com>
2017-01-21 18:15:03 -0800

    minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   root 'static_pages#root'
-
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :update, :index, :show]
     resource :session, only: [:create, :destroy]
